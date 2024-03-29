f73314751cac789b41c447f8e612dc4f9a0496f0
Ziad-Asr <ziadelsayedasr@gmail.com>
2024-03-29 14:10:45 +0200

    update

--- Backend/server.js
+++ Backend/server.js
@@ -30,6 +30,7 @@ app.use(express.static(path.join(__dirname, "public")));
 // 6) Settingup routes
 app.use("/", require("./routes/root"));
 app.use("/auth", require("./routes/authRoutes"));
+app.use("/users", require("./routes/usersRoutes"));
 
 app.all("*", (req, res) => {
   res.status(404);
