3b6f9de8750b74f741ed73c7caa5e7721575439a
Ziad-Asr <ziadelsayedasr@gmail.com>
2024-03-30 15:22:29 +0200

    update

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import {
-  Navigate,
+  // Navigate,
   Route,
   RouterProvider,
   createBrowserRouter,
   createRoutesFromElements,
 } from "react-router-dom";
-import Cookies from "js-cookie";
+// import Cookies from "js-cookie";
 
-import RequireAuth from "./components/auth/RequireAuth";
+// import RequireAuth from "./components/auth/RequireAuth";
 import RootLayout from "./components/RootLayout";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/auth/Login";
@@ -16,13 +16,21 @@ import Signup from "./pages/auth/Signup";
 import "./App.css";
 
 function App() {
-  const accessToken = Cookies.get("accessToken");
+  // const accessToken = Cookies.get("accessToken");
 
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<RootLayout />}>
         <Route index element={<h1>Authentication App</h1>} />
+
         <Route path="auth">
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<Signup />} />
+        </Route>
+
+        <Route path="/dashboard" element={<Dashboard />} />
+
+        {/* <Route path="auth">
           <Route
             path="login"
             element={
@@ -36,6 +44,7 @@ function App() {
             }
           />
         </Route>
+
         <Route
           path="/dashboard"
           element={
@@ -44,6 +53,7 @@ function App() {
             </RequireAuth>
           }
         />
+        <Route path="/dashboard" element={<Dashboard />} /> */}
       </Route>
     )
   );
